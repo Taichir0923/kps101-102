f12ef945948ae34f465bd5d2c525e96948f78770
taichir0923 <crystal19950923@gmail.com>
2021-10-11 20:54:25 +0800

    JS

---
+++ JavaScript/script.js
@@ -0,0 +1,101 @@
+// ES5
+// DataType - Primitive DataTypes
+// 1. String - text tovtei - "jgklhlkdfjlk 123 05460 true"
+// 2. Number - toon torliin data - 123456789
+// 3. Boolean - true, false [1, 0]
+// 4. undefined - todohoigui
+// 5. null - hooson
+
+// variable - хувьсагч
+// js reserved words
+// too bj bolohgui
+// toogoor ehelj bolohgui
+// var myVar = "gjfkljgd"
+// var number = 123456;
+// var bolean = true;
+
+// console.log(myVar);
+// Blocking
+// alert('jfdkkl1')
+// console.log('jgfkl')
+// alert('alert after console')
+// console.log('finished')
+
+// 
+
+// var x = 5;
+// var y = 6;
+// var temp = x;
+// x = y;
+// y = temp
+// var str = "23"
+
+// remainder operator
+// =
+// ==
+// ===
+// if statement
+// if else statement
+// if else if statement
+
+
+// 15 - 25 zaluu nas
+// 26 - 40 ider nas
+// 40 - 55 ahmad
+// 55 - ondor
+
+// loop 
+// huvisagch, condition, operator
+// counter
+// [], {}
+// for, for in, for of, while, do while
+// for(var i = 0; i <= 10; i++){
+//     console.log(i + ' hi')
+// }
+
+// var i = 0;
+// do {
+//     console.log(i + " hi")
+//     i++
+// } while(i < 10)
+
+// 100 hurtel toonoos buh 3t huvaagddag toog consold hevle
+
+// index 
+// for(var i = 0; i < 100; i++){
+//     if(i%3 === 0){
+//         console.log(i)
+//     }
+// }
+// let i = 0
+// do {
+//     if(i%3 === 0){
+//         console.log(i)
+//     }
+//     i++;
+// } while (i < 100)
+
+// let bla;
+// console.log(bla)
+var x = prompt('ehnii toog oruul');
+// var y = +prompt('2 dahi toog oruul');
+// var z = +prompt('3 dahi toog oruul');
+
+// var counter = 0;
+// if(x === 0) {
+//     counter++
+// }
+
+// if(y === 0) {
+//     counter++
+// }
+
+// if(z === 0) {
+//     counter++
+// }
+
+// // console.log(counter + ' shirheg 0-tei tentsuu too bna')
+// let revers = 0
+// for(var i = x.length - 1; i >= 0; i--){
+//     revers += +x[i];
+// }
