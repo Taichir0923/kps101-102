ba2f660ad0df3777b22b33880152fb045e82c7a3
taichir0923 <crystal19950923@gmail.com>
2021-11-01 20:09:31 +0800

    calculator

--- 10.29/script.js
+++ 10.29/script.js
@@ -2,7 +2,6 @@
 // document
 
 
-const input = document.querySelector('#input');
 // title.style.color = "yellow";
 // title.style.backgroundColor = "black";
 
@@ -16,14 +15,66 @@ const input = document.querySelector('#input');
 
 
 // function changeColor(){
-//     title.classList.toggle('yellow')
-//     title.innerHTML = input.value;
+    //     title.classList.toggle('yellow')
+    //     title.innerHTML = input.value;
+// }
+// const input = document.querySelector('#input');
+
+// let isEqual = false;
+    
+// function getValue(num){
+//     if(num === "="){
+//        input.value = eval(input.value);
+//        isEqual = true;
+//     } else if(
+//         (num === "+" || num === "-" || num === "*" || num === "/") && (
+//             input.value[input.value.length - 1] === "+" ||
+//             input.value[input.value.length - 1] === "-" ||
+//             input.value[input.value.length - 1] === "*" ||
+//             input.value[input.value.length - 1] === "/"
+//         )
+//     ){
+//         input.value = input.value.slice(0, input.value.length - 1);
+//         input.value += num
+//     } else {
+//         if(isEqual){
+//             if(num === "+" || num === "-" || num === "*" || num === "/"){
+//                 input.value += num;
+//             } else {
+//                 input.value = "";
+//                 input.value += num;
+//             }
+//             isEqual = false;
+//         } else { 
+//             input.value += num;
+//         }
+//     }
 // }
 
-function getValue(num){
-    if(num === "="){
-       input.value = eval(input.value)
-    } else {
-        input.value += num;
-    }
-}
+// SwopWatch
+
+setInterval((arg, arg1) => {
+    console.log(arg, arg1)
+}, 1000/60, "bnfdklghjfdslk", "value");
+// function testAnimation(){
+//     requestAnimationFrame(testAnimation);
+//     console.log("gjkfdlj")
+// }
+
+// testAnimation()
+// 60FPS
+
+// canvas
+
+
+
+// const dom = document.querySelector('#stop')
+// let doli = 0;
+// let minute = 0;
+// let hour = 0;
+// let second = 0;
+// setInterval(() => {
+//     console.log(second);
+//     second++;
+//     dom.innerHTML = second
+// }, 1000)
