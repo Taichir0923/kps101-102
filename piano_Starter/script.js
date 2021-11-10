7b091920689f3244368f6bb6700bd37704b4494b
taichir0923 <crystal19950923@gmail.com>
2021-11-10 18:46:30 +0800

    Update script.js

---
+++ piano_Starter/script.js
@@ -0,0 +1,18 @@
+// event listener
+// onclick
+const c4white = document.querySelector('#c4');
+const noteC4 = document.querySelector('#noteC4');
+
+document.addEventListener("keydown", function(e){
+    if(e.keyCode === 90){
+        c4white.classList.add('bg-gray-300')
+        noteC4.play()
+    }
+})
+
+document.addEventListener('keyup', function(e){
+    if(e.keyCode === 90){
+        c4white.classList.remove('bg-gray-300')
+        noteC4.currentTime = 0;
+    }
+})
