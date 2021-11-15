ae3cbbb388b8e0347fe2542591eacd649cf98fe8
taichir0923 <crystal19950923@gmail.com>
2021-11-15 19:53:06 +0800

    Update script.js

--- 11.15/script.js
+++ 11.15/script.js
@@ -2,14 +2,30 @@ const container = document.querySelector('.container #container');
 const input = document.querySelector('input[type="text"]')
 const btn = document.querySelector('input[type="button"]')
 
+const images = [];
+
 btn.addEventListener('click', function(){
-    container.insertAdjacentHTML('afterbegin', `
-        <div class="box">
-            <img src=${input.value} alt="">
-            <div id="delete" class="delete">
-                <i class="fas fa-times"></i>
+    images.push(input.value)
+    updateUI()
+})
+
+function updateUI(){
+    container.innerHTML = '';
+    images.forEach(function(img){
+        container.insertAdjacentHTML('afterbegin', `
+            <div class="box">
+                <img src=${img} alt="">
+                <div class="delete">
+                    <i id="delete" class="fas fa-times"></i>
+                </div>
             </div>
-        </div>
-    `)
+        `)
+    })
     input.value = ''
+}
+
+document.addEventListener('click', function(e){
+    if(e.target.id === "delete"){
+        e.target.parentNode.parentNode.remove()
+    }
 })
