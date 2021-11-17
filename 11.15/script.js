c4d01f250c3cd802c77ff66a7f9b9f710882557c
taichir0923 <crystal19950923@gmail.com>
2021-11-17 19:25:07 +0800

    Update script.js

--- 11.15/script.js
+++ 11.15/script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector('.container #container');
 const input = document.querySelector('.imgInput')
+const ner = document.querySelector('.ner')
 const btn = document.querySelector('input[type="button"]')
 const backdrop = document.querySelector('.backdrop');
 
@@ -15,8 +16,11 @@ const images = [];
  */
 
 btn.addEventListener('click', function(){
-    if(input.value.trim() !== ''){
-        images.push(input.value)
+    if(input.value.trim() !== '' && ner.value.trim() !== ""){
+        images.push({
+            zurag: input.value,
+            ner: ner.value
+        })
         updateUI()
     } else {
         alert('Өгөгдөл дутуу байна')
@@ -25,17 +29,18 @@ btn.addEventListener('click', function(){
 
 function updateUI(){
     container.innerHTML = '';
-    images.forEach(function(img , index){
+    images.forEach(function(object , index){
         container.insertAdjacentHTML('beforeend', `
             <div id="zurag-${index}" class="box">
-                <img src=${img} alt="">
+                <img src=${object.zurag} alt="">
                 <div class="delete">
                     <i id="delete" class="fas fa-times"></i>
                 </div>
             </div>
         `)
     })
-    input.value = ''
+    input.value = '';
+    ner.value = ''
 }
 
 document.addEventListener('click', function(e){
@@ -47,7 +52,7 @@ document.addEventListener('click', function(e){
     if(e.target.classList.contains('box')){
         const openIndec = +e.target.id.split('-')[1];
         backdrop.classList.add('open');
-        backdrop.children[0].children[0].src = images[openIndec];
+        backdrop.children[0].children[0].src = images[openIndec].zurag;
     }
     if(e.target.classList.contains('open')){
         backdrop.classList.remove('open');
