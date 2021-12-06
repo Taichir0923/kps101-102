275adbc8baf39d8d0442935fc3916df04c397f9d
taichir0923 <crystal19950923@gmail.com>
2021-12-06 18:43:59 +0800

    login

--- 12.03/script.js
+++ 12.03/script.js
@@ -3,6 +3,7 @@ const email = document.querySelector('#email');
 const number = document.querySelector('#number');
 const password = document.querySelector('#password');
 const register = document.querySelector('#register');
+const image = document.querySelector('#image');
 
 var users;
 
@@ -12,15 +13,21 @@ if(localStorage["users"]){
     users = [];
 }
 
+class User {
+    constructor(fullname, email, number, password , avatar){
+        this.fullname = fullname;
+        this.email = email;
+        this.number = number;
+        this.password = password;
+        this.avatar = avatar
+        this.id = Math.random().toString().split('.')[1];
+    }
+}
+
 register.addEventListener('click' , function(e){
     e.preventDefault();
     
-    const user = {
-        fullname: fullname.value,
-        email: email.value,
-        number: number.value,
-        password: password.value,
-    }
+    const user = new User(fullname.value, email.value , number.value, password.value, image.value);
 
     users.push(user);
     localStorage.setItem('users' , JSON.stringify(users));
